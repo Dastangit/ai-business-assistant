e8805deca849755623ae8b78da2bb34469b276d6
Dastan <paypaldastan@gmail.com>
2026-09-07 09:27:09 -0400

    clear-chat

--- dastan-x-tech/components/ChatWidget.jsx
+++ dastan-x-tech/components/ChatWidget.jsx
@@ -97,7 +97,7 @@ export default function ChatWidget() {
               onClick={handleClearChat}
               className="chat-clear-btn"
             >
-              🗑️ Limpiar
+              Limpiar
             </button>
           </div>
           
